8210bce99be4e792c25332b653830ecb8c14129e
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-08-11 23:29:35 +0100

    Stop generated ACHU description spam

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/config.ts'
-import { upgradeAchuDescriptions } from './achuDescriptionUpgradeSafe'
-
-upgradeAchuDescriptions()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
