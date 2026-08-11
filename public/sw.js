7f67cb02b046024e6e9089534244c5e9486dfbc8
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-08-11 13:37:11 +0100

    Restore update banner for installed app

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,16 @@
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(caches.open('tasks-calendar-v3').then((cache) => cache.addAll(['/', '/manifest.webmanifest', '/icon.svg'])));
+  event.waitUntil(caches.open('tasks-calendar-v4').then((cache) => cache.addAll(['/', '/manifest.webmanifest', '/icon.svg'])));
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== 'tasks-calendar-v3').map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== 'tasks-calendar-v4').map((key) => caches.delete(key)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
   event.respondWith(fetch(event.request).then((response) => {
     const copy = response.clone();
-    caches.open('tasks-calendar-v3').then((cache) => cache.put(event.request, copy));
+    caches.open('tasks-calendar-v4').then((cache) => cache.put(event.request, copy));
     return response;
   }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/'))));
 });
