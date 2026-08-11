0d987e0e03ca4bf6c63af44128592b5c415f5fa5
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-08-11 01:54:36 +0100

    Complete reliability audit fixes (public/sw.js)

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open('tasks-calendar-v2').then((cache) => cache.addAll(['/', '/manifest.webmanifest', '/icon.svg'])));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== 'tasks-calendar-v2').map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open('tasks-calendar-v2').then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/'))));
+});
+
 self.addEventListener('message', (event) => {
   if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
 });
