f2d63d09f722a79bd4affede3dc0f314e027e556
robertoiosifrbt-rgb <robertoiosifrbt@gmail.com>
2026-08-11 23:19:32 +0100

    Revert failing ACHU description startup import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/config.ts'
-import './achuDescriptionUpgrade.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
